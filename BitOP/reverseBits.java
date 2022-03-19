c9b13d6f605faf368042f7149be950fd7d93ae38
Haitao Huang <v0rt3xh@gmail.com>
2022-03-19 10:30:29 -0500

    Reverse Bits

---
+++ BitOP/reverseBits.java
@@ -0,0 +1,17 @@
+/* 
+0190 Reverse Bits
+*/
+
+// Bit manipulations 
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int rev = 0; // result
+        for (int i = 0; i < 32 && n != 0; ++i) {
+            rev |= (n & 1) << (31 - i);
+            n >>>= 1;
+        }
+        return rev;
+    }
+}
