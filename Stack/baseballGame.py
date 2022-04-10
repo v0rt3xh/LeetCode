be176f2e24bf3f88c5fad696c5317b1b442ad9ad
Haitao Huang <v0rt3xh@gmail.com>
2022-04-10 09:04:44 -0500

    Baseball Game

---
+++ Stack/baseballGame.py
@@ -0,0 +1,17 @@
+'''
+0682 Baseball Game
+Use stack to simulate the process
+'''
+class Solution:
+    def calPoints(self, ops: List[str]) -> int:
+        stack = []
+        for op in ops:
+            if op == 'C':
+                stack.pop()
+            elif op == 'D':
+                stack.append(stack[-1] * 2)
+            elif op == '+':
+                stack.append(stack[-1] + stack[-2])
+            else:
+                stack.append(int(op))
+        return sum(stack)
