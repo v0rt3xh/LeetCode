c71f8af400c33fd4580fc08ae4adcb9c6a79fcf8
Haitao Huang <v0rt3xh@gmail.com>
2022-04-20 21:43:35 -0500

    design hashtable

---
+++ HashTable/designHashTable.java
@@ -0,0 +1,76 @@
+/*
+0705 Design HashSet 
+'''
+'''
+First, my dumb set [0] * (10 ** 6 + 1)
+Improve: 
+Try split into different bucket
+e.g 10 linked list 
+bucket 1: sentinel -> 1 -> 3 -> 5 
+bucket 2: sentinel -> 2 -> 4 -> 6
+...
+Determine bucket k by hash key!
+Hash function, a lot theory behind it!
+Search in bucket, delete / remove
+'''
+*/
+class MyHashSet {
+    private static final int BASE = 769;
+    private LinkedList[] data;
+
+    /** Initialize your data structure here. */
+    public MyHashSet() {
+        data = new LinkedList[BASE];
+        for (int i = 0; i < BASE; ++i) {
+            data[i] = new LinkedList<Integer>();
+        }
+    }
+    
+    public void add(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                return;
+            }
+        }
+        data[h].offerLast(key);
+    }
+    
+    public void remove(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                data[h].remove(element);
+                return;
+            }
+        }
+    }
+    
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int hash(int key) {
+        return key % BASE;
+    }
+}
+'''
+Credit:
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/design-hashset/solution/she-ji-ha-xi-ji-he-by-leetcode-solution-xp4t/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
