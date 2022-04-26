96cf17fb37ae63994ea81b0af2f0382269ca75b2
Haitao Huang <v0rt3xh@gmail.com>
2022-04-25 19:19:12 -0500

    fix typo

--- LinkList/peekingIterator.py
+++ LinkList/peekingIterator.py
@@ -36,3 +36,25 @@ class PeekingIterator:
         :rtype: bool
         """
         return self.cursor + 1 < len(self.storage)
+
+'''
+Official Solution
+Leetcode-Solution @ Leetcode-cn
+'''
+class PeekingIterator:
+    def __init__(self, iterator):
+        self.iterator = iterator
+        self._next = iterator.next()
+        self._hasNext = iterator.hasNext()
+
+    def peek(self):
+        return self._next
+
+    def next(self):
+        ret = self._next
+        self._hasNext = self.iterator.hasNext()
+        self._next = self.iterator.next() if self._hasNext else 0
+        return ret
+
+    def hasNext(self):
+        return self._hasNext
